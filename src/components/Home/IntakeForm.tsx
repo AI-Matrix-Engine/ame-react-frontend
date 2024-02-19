11d6a4a4693bf3550c4db4d2caa27e3dbdfb378d
usmangq12 <usmangq12@gmail.com>
2024-02-20 03:53:40 +0500

    Removed unnessary files atm.

--- src/components/Home/IntakeForm.tsx
+++ src/components/Home/IntakeForm.tsx
@@ -102,7 +102,7 @@ export const IntakeForm = ({
             return (
               <div
                 key={index}
-                className="flex gap-4 md:flex-row items-start md:items-center border-b border-gray-300 p-4 mb-4 rounded mt-2"
+                className="flex gap-4 md:flex-column items-start md:items-center border-b border-gray-300 p-4 mb-4 rounded mt-2"
                 onMouseEnter={() => setHoveredIndex(index)}
                 onMouseLeave={() => setHoveredIndex(-1)}
               >
