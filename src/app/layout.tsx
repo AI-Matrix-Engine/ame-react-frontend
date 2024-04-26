8d2e633b587a115e95070a7382146306bad54cd9
smi0102 <wstar3233@gmail.com>
2024-04-26 17:03:16 +0800

    fix: loading spinner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,47 +3,17 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { Main } from "@/components/Home/Main";
-import { useEffect, useState } from "react";
-import Router from "next/router";
 const inter = Inter({ subsets: ["latin"] });
-
-import Spinner from "@/components/Spinner";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const startLoading = () => setLoading(true);
-    const endLoading = () => setLoading(false);
-    const handleError = (error: string) => setError(error);
-
-    Router.events.on("routeChangeStart", startLoading);
-    Router.events.on("routeChangeComplete", endLoading);
-    Router.events.on("routeChangeError", handleError);
-
-    return () => {
-      Router.events.off("routeChangeStart", startLoading);
-      Router.events.off("routeChangeComplete", endLoading);
-      Router.events.off("routeChangeError", handleError);
-    };
-  }, []);
-
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#f0f2f5]`}>
-        {error ? (
-          <div>{error}</div>
-        ) : (
-          <>
-            {loading && <Spinner />}
-            <Main>{children}</Main>
-          </>
-        )}
+        <Main>{children}</Main>
       </body>
     </html>
   );
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-screen h-screen flex justify-center items-center">
       <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     </div>
   );
