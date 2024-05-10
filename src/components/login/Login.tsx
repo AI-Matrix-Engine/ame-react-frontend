d0d8bcfcdad8b2582a6a84951220958e60e8d754
Hilarious95 <yummy.radish.x@gmail.com>
2024-05-10 13:45:27 +0900

    fix: loading spinner and the title of login page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -164,7 +164,7 @@ export const Login: NextPage = () => {
     <main className="p-4 font-poppins">
       <div className="mx-auto mt-10 w-[384px] bg-[#fff] rounded-lg overflow-hidden">
         <h1 className="text-[18px] text-center py-[16px] bg-[#252b36] text-white">
-          AIDRM
+          AI Matrix Engine
         </h1>
         <form
           className="w-[384px] max-w-md mx-auto p-[20px]"
